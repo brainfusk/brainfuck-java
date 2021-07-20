5ef61507122d4c7bccb560305313afc36eae31cf
techzealot <techzealot@foxmail.com>
2021-07-20 12:10:35 +0800

    add jit fragment

---
+++ brainfuck-jit/src/main/java/com/techzealot/brainfuck/jit/assembler/Assembler.java
@@ -0,0 +1,7 @@
+package com.techzealot.brainfuck.jit.assembler;
+
+/**
+ * 简单版本，只实现相关功能
+ */
+public class Assembler {
+}
---
+++ brainfuck-jit/src/main/java/com/techzealot/brainfuck/jit/assembler/DynAsm.java
@@ -0,0 +1,8 @@
+package com.techzealot.brainfuck.jit.assembler;
+
+/**
+ * TODO：参考dynasm实现，标签跳转难以在Java中实现
+ *
+ */
+public class DynAsm {
+}
